5f3dc4697e011912fec126f76a20b41135701ed7
FerAdriankw <60761543@continental.edu.pe>
2025-03-23 14:11:42 -0500

    Add files via upload

---
+++ PSeint1.psc
@@ -0,0 +1,3 @@
+Algoritmo PREGUNTA01
+	Escribir "Hola mundo, mi nombre es Padilla Calderon Fernando"
+FinAlgoritmo
---
+++ pregunta01.py
@@ -0,0 +1,15 @@
+#Elabora un programa y mostrar en ejecucion el siguiente mmensaje : Hola mundo,}
+#mi nombre es Fernando Adrian Padilla Calderon
+
+#ENTRADA
+
+#PROCESO
+import math
+varNumero = int(input("ingrese un numero: ")) # 8
+raizCubica = varNumero**(1/3)
+raizCubica = pow(varNumero,(1/3))
+#SALIDA
+print(raizCubica)
+
+
+
